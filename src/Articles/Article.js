8d07ef51fdc2a5ec33330fa3be948814f3bfcd90
Daniil <zelenko.danil@gmail.com>
2020-10-24 20:11:18 +0300

    Link to full article

--- src/Articles/Article.js
+++ src/Articles/Article.js
@@ -1,16 +1,19 @@
 import pt from 'prop-types';
 import React from "react";
+import { Link } from "react-router-dom";
 
 export function Article(props) {
     return (
         <div className="article_item">
             <h3>{ props.title }</h3>
             <p>{ props.text }</p>
+            <Link to={ "/article/" + props.article_id }>Go to article</Link>
         </div>
     )
 }
 
 Article.propTypes = {
+    article_id: pt.number,
     title: pt.string,
     text: pt.string
 };
